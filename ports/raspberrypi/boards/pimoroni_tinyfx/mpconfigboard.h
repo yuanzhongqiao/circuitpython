20d069ddf989f6e894b0087209c4c152cab7b583
Bernhard Bablok <bablokb@gmx.de>
2024-08-16 12:16:15 +0200

    initial version of board-definition for Pimoroni Tiny FX

---
+++ ports/raspberrypi/boards/pimoroni_tinyfx/mpconfigboard.h
@@ -0,0 +1,13 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#define MICROPY_HW_BOARD_NAME "Pimoroni Tiny FX"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO17)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO16)
