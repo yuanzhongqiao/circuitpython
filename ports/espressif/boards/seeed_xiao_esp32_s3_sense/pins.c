54b6d3d4a240e439a80f553249e34f1ae0240a29
Djair Guilherme <djair.jr@gmail.com>
2024-08-17 13:22:20 -0300

    Update pins.c
    Added objtuple to detect camera data tuple

--- ports/espressif/boards/seeed_xiao_esp32_s3_sense/pins.c
+++ ports/espressif/boards/seeed_xiao_esp32_s3_sense/pins.c
@@ -1,4 +1,6 @@
+#include "py/objtuple.h"
 #include "shared-bindings/board/__init__.h"
+#include "shared-module/displayio/__init__.h"
 
 static const mp_rom_obj_tuple_t camera_data_tuple = {
     // The order matters.
