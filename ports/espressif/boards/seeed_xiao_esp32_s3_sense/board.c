d037b9aa463da61d71dc4bf6ad70e8d14a66ea59
Djair Guilherme <djair.jr@gmail.com>
2024-08-17 21:04:36 -0300

    Update board.c
    Trying to deal with errors when creating espcamera object

--- ports/espressif/boards/seeed_xiao_esp32_s3_sense/board.c
+++ ports/espressif/boards/seeed_xiao_esp32_s3_sense/board.c
@@ -25,5 +25,9 @@
  */
 
 #include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/i2c.h"
 
 // Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
