53f1fdd18616b138d0753fae7311d98966e78186
Pontus Oldberg <pontus.oldberg@non.se.com>
2024-08-27 10:07:23 +0200

    Adds ilabs boards to the mix.

---
+++ ports/raspberrypi/boards/challenger_rp2350_wifi6_ble5/mpconfigboard.mk
@@ -0,0 +1,18 @@
+USB_VID = 0x2e8a
+USB_PID = 0x109a
+USB_PRODUCT = "Challenger+ RP2350 WiFi6/BLE5"
+USB_MANUFACTURER = "Invector Labs"
+
+CHIP_VARIANT = RP2350
+CHIP_PACKAGE = A
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q64JVxQ"
+
+CIRCUITPY__EVE = 1
+CIRCUITPY_ALARM = 0
+CIRCUITPY_PICODVI = 1
+
+FROZEN_MPY_DIRS += $(TOP)/ports/raspberrypi/boards/challenger_rp2350_wifi6_ble5
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register
