b1c428ae4071c51f646fb5a30f6a77ad11b01a6c
Scott Shawcroft <scott@tannewt.org>
2024-09-12 14:09:23 -0700

    Disable touchio on RP2350
    It doesn't work due to the E9 errata on A2 hardware.
    
    Fixes #9541

--- ports/raspberrypi/mpconfigport.mk
+++ ports/raspberrypi/mpconfigport.mk
@@ -51,13 +51,19 @@ CIRCUITPY_ALARM ?= 1
 
 # Default PICODVI off because it uses RAM to store code run on the second CPU for RP2040.
 CIRCUITPY_PICODVI ?= 0
+
+CIRCUITPY_TOUCHIO ?= 1
 endif
 
 ifeq ($(CHIP_VARIANT),RP2350)
 # This needs to be implemented.
-CIRCUITPY_ALARM ?= 0
+CIRCUITPY_ALARM = 0
 # Default PICODVI on because it doesn't require much code in RAM to talk to HSTX.
 CIRCUITPY_PICODVI ?= 1
+
+# Our generic touchio uses a pull down and RP2350 A2 hardware doesn't work correctly.
+# So, turn touchio off because it doesn't work.
+CIRCUITPY_TOUCHIO = 0
 endif
 
 INTERNAL_LIBM = 1
