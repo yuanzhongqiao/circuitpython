b026a410e0beb911904cb268a24d0a860856f0a9
Jeff Epler <jepler@gmail.com>
2024-09-13 10:29:52 -0500

    fix doc build

--- ports/raspberrypi/bindings/rp2pio/StateMachine.c
+++ ports/raspberrypi/bindings/rp2pio/StateMachine.c
@@ -21,6 +21,8 @@
 #include "py/objproperty.h"
 #include "py/runtime.h"
 
+//| import memorymap
+//|
 //| FifoType = Literal["auto", "txrx", "tx", "rx", "txput", "txget", "putget"]
 //| FifoType_piov0 = Literal["auto", "txrx", "tx", "rx"]
 //| MovStatusType = Literal["txfifo", "rxfifo", "irq"]
@@ -909,7 +911,7 @@ MP_DEFINE_CONST_FUN_OBJ_1(rp2pio_statemachine_get_pc_obj, rp2pio_statemachine_ob
 MP_PROPERTY_GETTER(rp2pio_statemachine_pc_obj,
     (mp_obj_t)&rp2pio_statemachine_get_pc_obj);
 
-//|     rxfifo: AddressRange
+//|     rxfifo: memorymap.AddressRange
 //|     """Accecss the state machine's rxfifo directly
 //|
 //|     If the state machine's fifo mode is ``txput`` then accessing this object
