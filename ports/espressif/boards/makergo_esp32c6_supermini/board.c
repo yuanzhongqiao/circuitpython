e9bb5cf4de62de7ca36d6c63e4ce53d10b11980b
Bill Sideris <bill88t@feline.gr>
2024-10-06 17:02:09 +0300

    Do power led

--- ports/espressif/boards/makergo_esp32c6_supermini/board.c
+++ ports/espressif/boards/makergo_esp32c6_supermini/board.c
@@ -5,5 +5,20 @@
 // SPDX-License-Identifier: MIT
 
 #include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "shared-bindings/microcontroller/Pin.h"
+#include "driver/gpio.h"
+#include "common-hal/microcontroller/Pin.h"
+
+bool espressif_board_reset_pin_number(gpio_num_t pin_number) {
+    if (pin_number == 15) {
+        /*
+         * Turn on the spare led on boot as a power indicator
+         */
+        config_pin_as_output_with_level(pin_number, true);
+        return true;
+    }
+    return false;
+}
 
 // Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
