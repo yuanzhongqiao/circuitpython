557ee26874f862250bc4a179b98fffc98e751506
Sylwester <6614616+DatanoiseTV@users.noreply.github.com>
2024-10-08 20:48:47 +0200

    Update ports/raspberrypi/boards/datanoise_picoadk_v2/mpconfigboard.h
    Co-authored-by: Dan Halbert <halbert@halwitz.org>

--- ports/raspberrypi/boards/datanoise_picoadk_v2/mpconfigboard.h
+++ ports/raspberrypi/boards/datanoise_picoadk_v2/mpconfigboard.h
@@ -13,9 +13,9 @@
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO9)
 #define DEFAULT_I2C_BUS_SDA (&pin_GPIO8)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
-#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO12)
-#define DEFAULT_SPI_BUS_MISO (&pin_GPIO20)
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO6)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO3)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO4)
 
 #define DEFAULT_UART_BUS_RX (&pin_GPIO13)
 #define DEFAULT_UART_BUS_TX (&pin_GPIO12)
