b8d25e59c4e220bf8525b6272418e248bfeb822d
DatanoiseTV <github@ext.no-route.org>
2024-10-08 18:32:35 +0200

    Remove old pins from Datanoise PicoADK V2.

--- ports/raspberrypi/boards/datanoise_picoadk_v2/pins.c
+++ ports/raspberrypi/boards/datanoise_picoadk_v2/pins.c
@@ -52,11 +52,6 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
-    { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_D26), MP_ROM_PTR(&pin_GPIO26) },
-    { MP_ROM_QSTR(MP_QSTR_D27), MP_ROM_PTR(&pin_GPIO27) },
-    { MP_ROM_QSTR(MP_QSTR_D28), MP_ROM_PTR(&pin_GPIO28) },
-    { MP_ROM_QSTR(MP_QSTR_D29), MP_ROM_PTR(&pin_GPIO19) }
+    { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) }
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
